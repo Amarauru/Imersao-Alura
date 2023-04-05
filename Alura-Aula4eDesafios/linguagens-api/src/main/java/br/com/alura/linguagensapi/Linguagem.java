51717d1605f1cf0c9857ee0703523b499def8911
Amarauru <pedrohaa11@gmail.com>
2023-04-05 00:42:31 -0300

    Alura-Aula 4 e Desafios CRUD

---
+++ Alura-Aula4eDesafios/linguagens-api/src/main/java/br/com/alura/linguagensapi/Linguagem.java
@@ -0,0 +1,53 @@
+package br.com.alura.linguagensapi;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Document(collection ="principaisLinguagens")
+public class Linguagem {
+
+
+@Id
+
+    private String id;
+    private String tittle;
+    private String image;
+    private int ranking;
+    
+
+    public Linguagem(){
+
+
+    }
+
+    
+    public Linguagem(String tittle, String image, int ranking) {
+        this.tittle = tittle;
+        this.image = image;
+        this.ranking = ranking;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+    public String getImage() {
+        return image;
+    }
+    public int getRanking() {
+        return ranking;
+    }
+public void setId(String id) {
+    this.id = id;
+}
+    
+
+
+
+
+    
+}
